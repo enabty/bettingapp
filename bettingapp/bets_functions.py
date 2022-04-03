8aa3ccfc0fcd62c8306fd5e2302595ee782ed7ab
ybrlap <ybringdrive@gmail.com>
2022-04-03 19:22:18 +0200

    groundwork done with funcs

--- bettingapp/bets_functions.py
+++ bettingapp/bets_functions.py
@@ -5,9 +5,9 @@ from auth import gspread
 from auth import sh
 
 
-def clean_cell(cell: str):
+def clear_cell(cell: str):
     sh.update(cell, "")
-    print(f"cleaned cell: {cell}")
+    print(f"Cleared cell [{cell}]")
 
 
 def get_cell_lst(cell: str) -> ValueRange:
@@ -18,7 +18,7 @@ def get_cell_val(cell: str) -> str:
     return sh.acell(cell).value
 
 
-def get_cell_format(cell: str) -> str:
+def get_cell_formula(cell: str) -> str:
     return sh.acell(cell, value_render_option='FORMULA').value
 
 
@@ -30,8 +30,27 @@ def get_row_val(row: int) -> list:
     return sh.row_values(row)
 
 
+def update_cell(cell: str, new_string: str):
+    """Updates a cell with a new value. Ex: update_cell('A1', 'foo') """
+    old_val = get_cell_val(cell)
+    sh.update(cell, new_string)
+    print(f'Updated cell [{cell}] from "{old_val}" --> "{new_string}"')
+
+
+def cell_isempty(cell: str) -> bool:
+    return get_cell_val(cell) == "" or get_cell_val(cell) is None
+
+
+def testing(cell):
+    print(cell_isempty(cell))
+    print(get_cell_val(cell))
+
+
 if __name__ == '__main__':
-    print(get_row_val(14))
+    testing("D17")
+
+
+
 
 
 
